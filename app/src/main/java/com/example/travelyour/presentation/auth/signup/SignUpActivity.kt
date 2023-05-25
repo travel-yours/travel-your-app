65f0db599104e0edb8ad81c618f4df5a077b2c6f
wahyu dev <wahyu05ningrat@gmail.com>
2023-05-25 20:54:00 +0700

    add file sign in layout

---
+++ app/src/main/java/com/example/travelyour/presentation/auth/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.travelyour.presentation.auth.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.travelyour.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
